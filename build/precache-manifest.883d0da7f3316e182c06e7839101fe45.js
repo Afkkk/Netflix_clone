c0e8e6f79790423a8b8077abf2f2eea6b4d89a40
Afkkk <47856497+Afkkk@users.noreply.github.com>
2020-10-02 22:55:52 +0200

    Add files via upload

---
+++ build/precache-manifest.883d0da7f3316e182c06e7839101fe45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbf65883ffcbc4220f9bb67708f7649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "832745908887bccc9ad2",
+    "url": "/static/css/main.5adbb0c2.chunk.css"
+  },
+  {
+    "revision": "11740604f59f90cb5e07",
+    "url": "/static/js/2.c1cfb421.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.c1cfb421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832745908887bccc9ad2",
+    "url": "/static/js/main.9db7c6b6.chunk.js"
+  },
+  {
+    "revision": "439ae095c7fe9cd37d99",
+    "url": "/static/js/runtime-main.0ea695f4.js"
+  }
+]);
